e0338c47ee87fc529fde9afba004e7b4fe72a9c1
jimenamontes07 <jimenamontes07@gmail.com>
2023-08-25 20:04:51 +0100

    Update CurrentTemp.js

--- src/CurrentTemp.js
+++ src/CurrentTemp.js
@@ -3,48 +3,24 @@ import { useEffect } from "react";
 
 export default function CurrentTemp(props){
 
-    let [temp, setTemp] = useState(props.temp);
+    //let [temp, setTemp] = useState(props.temp);
 
   
 
-    MyComponent();
-
- 
-
-    function MyComponent() {
-       useEffect(() => {
-          loadData();
-       }, []); // Pass an empty array to only call the function once on mount.
-       
-       function loadData() {
-    
-   
-        while( props.submit === true || props.submit ==="start"){
-            
-            setTemp(props.temp);
-        }
-        console.log(temp);
-     
-       
-       }
-       
-       // ... component render logic
-    }
-
 
   
-    function toFarhenheit(event) {
-       event.preventDefault();
-        let temperature = Math.round(temp * (9 / 5) + 32);
-        setTemp(temperature);
-      }
+    //function toFarhenheit(event) {
+      // event.preventDefault();
+        //let temperature = Math.round(temp * (9 / 5) + 32);
+        //setTemp(temperature);
+      //}
 
-    function toCelcius(event) {
-        event.preventDefault();
-       let temperature = props.temp;
-        setTemp(temperature);
+    //function toCelcius(event) {
+       // event.preventDefault();
+      // let temperature = props.temp;
+       // setTemp(temperature);
    
-      }
+     // }
 
 
 return(
@@ -55,7 +31,7 @@ return(
             <div className="col-6 mainTemp">
             <div>
                 <img className="icon" src={props.emoji} alt= {props.desc} />{" "} 
-                <span className="Temp">{Math.round(temp)}</span>
+                <span className="Temp">{Math.round(props.temp)}</span>
                 <span className="buttonsCelFar">
                 <button className="btn btn-light" id="toCelcius" onClick={toCelcius}>
                 {" "}
