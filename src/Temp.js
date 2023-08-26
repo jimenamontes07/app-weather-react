51f22fd27bf55a5be941bb67fca9e96e6eb0f6f0
jimenamontes07 <jimenamontes07@gmail.com>
2023-08-26 11:45:20 +0100

    btn design

--- src/Temp.js
+++ src/Temp.js
@@ -7,14 +7,21 @@ export default function Temp(props){
 
     let [celcius , setCelcius] = useState(true);
 
+    let [colorC , setColorC] = useState("#0C6EFD");
+    let [colorF , setColorF] = useState("black");
+
 
     if (celcius === true && temp !== props.temp){
         setTemp(props.temp);
+        setColorC("#0C6EFD");
+        setColorF("black");
     } 
   
     function toFarhenheit(event) {
       event.preventDefault();
       setCelcius(false);
+      setColorC("black");
+      setColorF("#0C6EFD");
         let temperature = Math.round(temp * (9 / 5) + 32);
         setTemp(temperature);
       }
@@ -30,12 +37,12 @@ export default function Temp(props){
         <span className="Temp">{Math.round(temp)}</span>
 
         <span className="buttonsCelFar">
-        <button className="btn btn-light" id="toCelcius" onClick={toCelcius}>
+        <button className="btn btn-light" id="toCelcius" onClick={toCelcius} style={{color: colorC }} >
         {" "}
         °C{" "}
         </button>
 
-        <button className="btn btn-light" id="toFarenheit" onClick={toFarhenheit} >
+        <button className="btn btn-light" id="toFarenheit" onClick={toFarhenheit} style={{color: colorF }}>
         {" "}
         °F{" "}
          </button>
